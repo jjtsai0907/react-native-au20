9fa4bc2c3c8f4c1b4a83c73d916b91e164ee1d73
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-12-02 08:35:39 +0100

    Update Home- and EditProductScreen UI

--- models/Item.tsx
+++ models/Item.tsx
@@ -1,19 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Product } from "./ProductObject";
 import React, { FC, useState } from "react";
 
 const Item: FC<Product> = (props) => {
   return (
-    <View>
-      <View>
-        <Text>Product Name: {props.name}</Text>
+    <View style={styles.listItemContainer}>
+      <View style={styles.itemDirection}>
+        <Text style={styles.leftItem}>Product: {props.name}</Text>
       </View>
       <View>
-        <Text>Product Price: {props.price}</Text>
-        <Text>Product Type: {props.productType}</Text>
+        <Text style={styles.rightItem}>Price: {props.price}</Text>
+        <Text style={styles.centerItem}>Type: {props.productType}</Text>
       </View>
     </View>
   );
 };
 
 export default Item;
+
+const styles = StyleSheet.create({
+  itemDirection: {},
+
+  leftItem: {
+    textAlign: "left",
+    padding: 10,
+  },
+  centerItem: {
+    textAlign: "center",
+    padding: 10,
+  },
+  rightItem: {
+    textAlign: "right",
+    padding: 10,
+  },
+  listItemContainer: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "lightgray",
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+    justifyContent: "space-between",
+  },
+});
--- screens/EditProductScreen.tsx
+++ screens/EditProductScreen.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Picker } from "react-native";
+import { Button } from "react-native-elements";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Product } from "../models/ProductObject";
+import { Feather, Foundation } from "@expo/vector-icons";
 
 const EditProductScreen: React.FC = (props: any) => {
   const [selectedProduct, setSelectedProduct] = useState<Product>();
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [productType, setProductType] = useState("Peripheral");
 
   useEffect(() => {
     (() => {
@@ -27,10 +32,50 @@ const EditProductScreen: React.FC = (props: any) => {
       alert("E Reading data error!" + error);
     }
   };
-
-  return (
-    <View>
+  /*
+<View>
       <Text>{selectedProduct?.name}</Text>
+    </View>
+*/
+  return (
+    <View style={styles.container}>
+      <Text>Create New Product</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Name"
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setPrice}
+        placeholder="Price"
+        keyboardType="numeric"
+      />
+
+      <Picker
+        style={styles.productPicker}
+        selectedValue={productType}
+        onValueChange={(itemValue, itemIndex) => setProductType(itemValue)}
+      >
+        <Picker.Item label="Peripheral" value="Peripheral" />
+        <Picker.Item label="Integrated" value="Integrated" />
+      </Picker>
+      <View style={styles.buttonContainer}>
+        <Button
+          icon={<Feather name="download" size={24} color="black" />}
+          title="SAVE"
+          disabled={
+            name != "" && price != "" && productType != "" ? false : true
+          }
+        />
+
+        <Button
+          icon={<Foundation name="prohibited" size={24} color="black" />}
+          title="CANCEL"
+          onPress={() => props.navigation.navigate("Home", { id: 1 })}
+        />
+      </View>
     </View>
   );
 };
@@ -66,42 +111,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
   },
-  saveButton: {
-    backgroundColor: "green",
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 15,
-    padding: 10,
-    justifyContent: "space-between",
-    flexDirection: "row",
-    width: "40%",
-  },
-  saveButtonDisabled: {
-    backgroundColor: "lightgray",
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 15,
-    padding: 10,
-    justifyContent: "space-between",
-    flexDirection: "row",
-    width: "40%",
-  },
-  saveButtonLabel: {
-    color: "white",
-    fontSize: 16,
-  },
-  cancelButton: {
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 15,
-    padding: 10,
-    flexDirection: "row",
-    width: "40%",
-    justifyContent: "space-between",
-  },
-  cancelButtonLabel: {
-    fontSize: 16,
-  },
+
   errorMessage: {
     marginHorizontal: 40,
     marginTop: 5,
